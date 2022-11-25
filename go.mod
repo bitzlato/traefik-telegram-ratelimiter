2a85dc40f0acee1f2fa8ee9a797795406ee38b8e
Alexey Khoroshev <aleksxor@gmail.com>
2022-11-25 18:32:16 +0300

    add basic rate limiter structure [wip]

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/bitzlato/traefik-telegram-ratelimit
+module github.com/bitzlato/traefik_telegram_ratelimiter
 
 go 1.18
--- tgratelimiter.go
+++ tgratelimiter.go
@@ -1,32 +1,66 @@
+// Package traefik_telegram_ratelimiter implements a rate limiting middleware based on messages' telegram ids
 package traefik_telegram_ratelimiter
 
 import (
 	"context"
 	"net/http"
+	"sync"
+	"time"
 )
 
+const defaultHitMapSize = 50000
+
 // Config holds configuration to pass to the plugin
-type Config struct{}
+type Config struct {
+	HitMapSize     uint32
+	Limit          int32
+	WhitelistLimit int32
+	TTL            time.Duration
+	Whitelist      *string
+	Blacklist      *string
+}
 
 // CreateConfig populates the Config data object
 func CreateConfig() *Config {
-	return &Config{}
+	return &Config{
+		HitMapSize:     defaultHitMapSize,
+		Limit:          -1,
+		WhitelistLimit: -1,
+		TTL:            time.Duration(24) * time.Hour, // 24 hours
+	}
 }
 
-// RateLimiter holds the necessary components of a Traefik plugin
-type RateLimiter struct {
+// rateLimiter implements rate limiting with a set of tocken buckets;
+type rateLimiter struct {
 	next http.Handler
 	name string
+	// ttl of the hit record
+	ttl time.Duration
+	// maximum hits limit
+	limit int32
+	// whitelist limit
+	wlLimit int32
+	// whitelist bst
+	whitelist map[int64]struct{}
+	// blacklist bst
+	blacklist map[int64]struct{}
+	// hits map
+	hits map[int64]uint32
+	// expire times map
+	expiry map[int64]uint32
+	mu     sync.Mutex
 }
 
 // New instantiates and returns the required components used to handle a HTTP request
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	return &RateLimiter{
+
+	return &rateLimiter{
 		next: next,
 		name: name,
+		mu:   sync.Mutex{},
 	}, nil
 }
 
-func (rl *RateLimiter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	rl.next.ServeHTTP(w, r)
+func (r *rateLimiter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	r.next.ServeHTTP(rw, req)
 }
